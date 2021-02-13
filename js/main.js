e1c9cd6f3a29f364133894cdd89eb52e97bd95a3
extremecode0xFF <77740243+extremecode0xFF@users.noreply.github.com>
2021-02-13 21:02:39 +0200

    carousel 0.8
    -add carousel
    -remove old ovals images
    -remove side arrays

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        center: true,
+        singleItem: true,
+        autoplay: true,
+        loop: true,
+        autoplayTimeout: 5500,
+        smartSpeed: 1000,
+        nav: true,
+        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
 
+    });
 });
